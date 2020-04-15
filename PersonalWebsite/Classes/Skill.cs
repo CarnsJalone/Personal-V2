51c920eabdb4fed2b24931bfa51fb1957352a888
Jarret Laberdee <Jarret Laberdee@DESKTOP-KKRCQVH>
2020-04-14 20:48:18 -0400

    First push

---
+++ PersonalWebsite/Classes/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalWebsite.Classes {
+    public class Skill {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Logo { get; set; }
+        public DateTime StartDate { get; set; }
+        public double TimeInvested { get {
+                return (DateTime.Now.Date - StartDate.Date).TotalDays;
+            }
+        }
+    }
+}
